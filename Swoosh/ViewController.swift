53ab8a2095aa18a8e55a9969714c60ffa71c1037
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-19 23:43:50 -0700

    Autolayout fun!!!

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgimg: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
     }
 
     override func didReceiveMemoryWarning() {
