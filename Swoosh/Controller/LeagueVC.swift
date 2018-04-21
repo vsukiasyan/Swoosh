a5bd2d7d946a3360c88d344dd18dea734382d75c
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-20 17:52:43 -0700

    Added a couple of VCs and data model.

--- Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Controller/LeagueVC.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
+    
+    @IBAction func onNextTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
+    }
+    
+    @IBAction func onMensTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "Mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "Womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "Coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,14 +49,20 @@ class LeagueVC: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player  
+        }
     }
-    */
+    
 
+    
+    
+    
 }
---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Vic Sukiasyan on 4/20/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
